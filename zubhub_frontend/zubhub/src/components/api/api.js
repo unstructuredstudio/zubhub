5d0c37ee5833e010425613a62486a20bede628ca
Ndibe Raymond <ndiberaymond1@gmail.com>
2020-12-14 21:06:42 +0100

    patch

--- zubhub_frontend/zubhub/src/components/api/api.js
+++ zubhub_frontend/zubhub/src/components/api/api.js
@@ -1,10 +1,6 @@
 class API {
   constructor(){
     this.domain = `http://${proccess.env.NODE_ENV === 'production' ? `167.71.30.125:8000`:`127.0.0.1:8000`}/api/`;
-
-    const BASE_API_URI = process.env.NODE_ENV === 'production' ?
-                     'https://contraband.toolforge.org/'
-                  :  'http://127.0.0.1:8000/';
   }
 
   request=({url = '/', method = 'GET', token, body})=>{
