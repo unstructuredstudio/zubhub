9fc089e3fea0effc3d9319f39a093f528490ac99
Ndibe Raymond <ndiberaymond1@gmail.com>
2020-12-15 23:28:40 +0100

    added deployment scripts and instructions on how to run them

--- zubhub_frontend/zubhub/deploy_frontend.sh
+++ zubhub_frontend/zubhub/deploy_frontend.sh
@@ -1,15 +1,12 @@
 #! /bin/bash
 
-# ssh -o "StrictHostKeyChecking=no" -i deploy_rsa -A ${DEPLOY_USERNAME}@${DEPLOY_HOST} << EOT
-# git clone https://github.com/unsctructuredstudio/zubhub/
-echo "Cloned the repository"
-
 mv zubhub_frontend/zubhub/.env zubhub/zubhub_frontend/zubhub/.env
 rm -rf zubhub_frontend
 mkdir zubhub_frontend
 cp -r zubhub/zubhub_frontend/* zubhub_frontend/
 rm -rf zubhub/ zubhub_frontend/zubhub/.env.example
 cd zubhub_frontend/zubhub/
+docker-compose down
 docker-compose up -d --build
 echo "Updated frontend"
 # EOT
