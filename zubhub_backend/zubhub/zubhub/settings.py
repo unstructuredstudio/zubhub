e4d8c8a15bef7f7cf4c08e71a69b3b82acd298d7
Ndibe Raymond Olisaemeka <NdibeRaymond1@gmail.com>
2020-12-20 05:02:17 +0100

    UI feature (#10)
    * redesigned the project details page
    
    * more ui changes
    
    * added functionality to replace video with image if user didn't submit video url
    
    * forced project card to have 0 padding bottom
    
    * made modifications to the ui in reference to issue #9

--- zubhub_backend/zubhub/zubhub/settings.py
+++ zubhub_backend/zubhub/zubhub/settings.py
@@ -226,6 +226,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_FINDER = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
---
+++ zubhub_frontend/zubhub/src/assets/js/muiTheme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Raleway',
+        'Roboto',
+        'sans-serif'
+      ].join(','),
+    }
+  });
