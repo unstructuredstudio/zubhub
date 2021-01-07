8c252de70d198db45bbcf65d8aa98d48674c5ebf
Ndibe Raymond Olisaemeka <NdibeRaymond1@gmail.com>
2021-01-07 01:39:53 +0100

    phase2 patial ----- 2 (#65)
    switched handling of ssl back to valian/docker-nginx-auto-ssl

--- zubhub_frontend/zubhub/deploy_frontend.sh
+++ zubhub_frontend/zubhub/deploy_frontend.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 mv zubhub_frontend/zubhub/.env zubhub/zubhub_frontend/zubhub/.env
-mv zubhub_frontend/zubhub/.zero-ssl zubhub/zubhub_frontend/zubhub/
+mv zubhub_frontend/zubhub/.ssl-data zubhub/zubhub_frontend/zubhub/
 rm -rf zubhub_frontend
 cp -r zubhub/zubhub_frontend/ zubhub_frontend/
 rm -rf zubhub/ zubhub_frontend/zubhub/.env.example
