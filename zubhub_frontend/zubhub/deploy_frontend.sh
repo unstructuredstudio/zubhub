58c463d103debc2739052d236bcb21cfd86a91ae
Ndibe Raymond Olisaemeka <NdibeRaymond1@gmail.com>
2021-01-25 19:56:56 +0100

    Update deploy_frontend.sh

--- zubhub_frontend/zubhub/deploy_frontend.sh
+++ zubhub_frontend/zubhub/deploy_frontend.sh
@@ -8,16 +8,16 @@ echo "done copying .env file and ssl folder"
 echo "removing unneccessary files and folders"
 rm -rf zubhub_frontend
 cp -r zubhub/zubhub_frontend/ zubhub_frontend/
-rm -rf zubhub/ zubhub_frontend/zubhub/.env.example
-rm -rf zubhub/ zubhub_frontend/zubhub/Dockerfile
-rm -rf zubhub/ zubhub_frontend/zubhub/docker-compose.yml
-rm -rf zubhub/ zubhub_frontend/zubhub/nginx/dev
+rm -rf zubhub_frontend/zubhub/.env.example
+rm -rf zubhub_frontend/zubhub/Dockerfile
+rm -rf zubhub_frontend/zubhub/docker-compose.yml
+rm -rf zubhub_frontend/zubhub/nginx/dev
 echo "done removing unneccessary files and folders"
 
 cd zubhub_frontend/zubhub/
 
 echo "stopping and rebuilding the containers"
 docker-compose down
-docker-compose -f ./docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml up -d --build
 echo "Updated frontend"
 # EOT
