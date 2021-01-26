83f9156c5c63add4c00a0ac093e142b02830d13b
Ndibe Raymond Olisaemeka <NdibeRaymond1@gmail.com>
2021-01-26 04:54:56 +0100

    Update deploy_frontend.sh

--- zubhub_frontend/zubhub/deploy_frontend.sh
+++ zubhub_frontend/zubhub/deploy_frontend.sh
@@ -1,22 +1,23 @@
 #! /bin/bash
 
 echo "copying .env file and ssl folder"
-mv /home/zubhub-frontend/zubhub_frontend/zubhub/.env /home/zubhub-frontend/zubhub/zubhub_frontend/zubhub/.env
-mv /home/zubhub-frontend/zubhub_frontend/zubhub/.ssl-data /home/zubhub-frontend/zubhub/zubhub_frontend/zubhub/
+mv zubhub_frontend/zubhub/.env zubhub/zubhub_frontend/zubhub/.env
+mv zubhub_frontend/zubhub/.ssl-data zubhub/zubhub_frontend/zubhub/
 echo "done copying .env file and ssl folder"
 
 echo "removing unneccessary files and folders"
-rm -rf /home/zubhub-frontend/zubhub_frontend
-cp -r /home/zubhub-frontend/zubhub/zubhub_frontend/ /home/zubhub-frontend/zubhub_frontend/
-rm -rf /home/zubhub-frontend/zubhub_frontend/zubhub/.env.example
-rm -rf /home/zubhub-frontend/zubhub_frontend/zubhub/Dockerfile
-rm -rf /home/zubhub-frontend/zubhub_frontend/zubhub/docker-compose.yml
-rm -rf /home/zubhub-frontend/zubhub_frontend/zubhub/nginx/dev
+rm -rf zubhub_frontend
+cp -r zubhub/zubhub_frontend/ zubhub_frontend/
+rm -rf zubhub_frontend/zubhub/.env.example
+rm -rf zubhub_frontend/zubhub/Dockerfile
+rm -rf zubhub_frontend/zubhub/docker-compose.yml
+rm -rf zubhub_frontend/zubhub/nginx/dev
 echo "done removing unneccessary files and folders"
 
+cd zubhub_frontend/zubhub/
 
 echo "stopping and rebuilding the containers"
-docker-compose -f /home/zubhub-frontend/zubhub_frontend/zubhub/docker-compose.prod.yml down
-docker-compose -f /home/zubhub-frontend/zubhub_frontend/zubhub/docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml up -d --build
 echo "Updated frontend"
 # EOT
