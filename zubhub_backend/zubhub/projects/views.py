180755758f2a8ef1de18baec02ab31253a612ca0
Ndibe Raymond Olisaemeka <NdibeRaymond1@gmail.com>
2021-02-24 12:38:54 +0100

    Fixed issue #112 (#113)

--- zubhub_backend/zubhub/projects/views.py
+++ zubhub_backend/zubhub/projects/views.py
@@ -17,6 +17,7 @@ class ProjectCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
+        self.request.user.save()
 
 
 class ProjectUpdateAPIView(UpdateAPIView):
