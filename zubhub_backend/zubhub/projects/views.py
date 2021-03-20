40afecbf6d518594451f70dc7aef4630650504da
Ndibe Raymond <ndiberaymond1@gmail.com>
2021-03-20 23:39:30 +0100

    fixed issue #118: implemented the moderator feature

--- zubhub_backend/zubhub/projects/views.py
+++ zubhub_backend/zubhub/projects/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import AnonymousUser
 from rest_framework import status
 from rest_framework.generics import UpdateAPIView, CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
-from projects.permissions import IsOwner
-from .models import Project
+from projects.permissions import IsOwner, IsStaffOrModerator
+from .models import Project, Comment
 from .serializers import ProjectSerializer, ProjectListSerializer, CommentSerializer
 from .pagination import ProjectNumberPagination
 
@@ -148,3 +148,26 @@ class AddCommentAPIView(CreateAPIView):
 
         result = self.get_object()
         return Response(ProjectSerializer(result).data, status=status.HTTP_201_CREATED)
+
+
+class UnpublishCommentAPIView(UpdateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrModerator]
+
+    def perform_update(self, serializer):
+        comment = serializer.save(published=False)
+        comment.project.save()
+        return comment
+
+
+class DeleteCommentAPIView(DestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsStaffOrModerator]
+
+    def delete(self, request, *args, **kwargs):
+        project = self.get_object().project
+        result = self.destroy(request, *args, **kwargs)
+        project.save()
+        return result
