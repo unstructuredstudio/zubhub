4aba119ac6a6a0f051b683ba892cef8a205c341b
Aditya Jain <adityaj5@illinois.edu>
2022-02-16 16:37:55 -0600

    Add boilerplate stuff

--- zubhub_backend/zubhub/notifications/models.py
+++ zubhub_backend/zubhub/notifications/models.py
@@ -6,7 +6,7 @@ import uuid
 from creators.models import Creator 
 
 class Notification(models.Model):
-    id = models.UUIDField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     message = models.TextField(blank=False, null=True)
     recipient = models.ForeignKey(Creator, on_delete = models.CASCADE, null=True, related_name="notifications", blank=True)
     viewed = models.BooleanField(default=False)
