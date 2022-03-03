757ae7664cc5bca0d9d8e0b8ade2e640a896054c
AndrewLester <alester220@gmail.com>
2022-03-02 21:51:37 -0600

    Add notification type field to notification model

--- zubhub_backend/zubhub/notifications/models.py
+++ zubhub_backend/zubhub/notifications/models.py
@@ -6,9 +6,17 @@ from creators.models import Creator
 
 
 class Notification(models.Model):
+    class Type(models.IntegerChoices):
+        CLAP = 1
+        COMMENT = 2
+        SAVE = 3
+        FOLLOW = 4
+
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True)
-    message = models.TextField(blank=False, null=True)
+    type = models.PositiveSmallIntegerField(
+        choices=Type.choices,
+    )
     recipient = models.ForeignKey(
         Creator, on_delete=models.CASCADE, null=True, related_name="notification_recipient", blank=True)
     source = models.ForeignKey(
