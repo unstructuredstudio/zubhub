4b007f04b155839c9424529f07c5eb332e3214b4
AndrewLester <alester220@gmail.com>
2022-03-02 22:17:09 -0600

    Add viewed to notification serializer

--- zubhub_backend/zubhub/notifications/serializers.py
+++ zubhub_backend/zubhub/notifications/serializers.py
@@ -9,8 +9,8 @@ class NotificationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Notification
-        fields = ('id', 'recipient', 'source', 'date', 'type')
+        fields = ('id', 'recipient', 'source', 'date', 'viewed', 'type')
 
         read_only_field = [
-            'id', 'recipient', 'source', 'date', 'type'
+            'id', 'recipient', 'source', 'date', 'viewed', 'type'
         ]
