9ca288ca271796fbf6beca04d4708692fc67c823
Grace Zhang <gracewzhang01@gmail.com>
2022-03-10 11:04:24 -0600

    Create projects & drafts tab

---
+++ zubhub_frontend/zubhub/src/assets/js/styles/components/projects_drafts/projectsDraftsGridStyles.js
@@ -0,0 +1,21 @@
+const styles = theme => ({
+    titleStyle: {
+      fontWeight: 900,
+      fontSize: '1.5rem',
+    },
+
+    profileLowerStyle: {
+      margin: '1em',
+    },
+
+    viewAllBtn: { 
+      float: 'left',
+      marginLeft: '2em',
+    },
+
+    gridStyle: {
+      marginBottom: '2em',
+    },
+});
+
+export default styles;
--- zubhub_frontend/zubhub/src/components/projects_drafts/ProjectsDraftsGrid.jsx
+++ zubhub_frontend/zubhub/src/components/projects_drafts/ProjectsDraftsGrid.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import styles from '../../assets/js/styles/views/profile/profileStyles';
+import styles from '../../assets/js/styles/components/projects_drafts/projectsDraftsGridStyles';
 import commonStyles from '../../assets/js/styles';
 import cn from 'classnames';
+import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import CustomButton from '../../components/button/Button';
 import {
   Typography,
   Grid,
   Paper,
+  Tabs, 
+  Tab,
+  Box,
 } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
@@ -22,6 +26,40 @@ import {updateProjects} from '../../views/profile/profileScripts'
 const useStyles = makeStyles(styles);
 const useCommonStyles = makeStyles(commonStyles);
 
+const TabPanel = (props)  => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+
 const ProjectsDraftsGrid = ({ props, state, profile, projects }) => {
   const classes = useStyles();
   const common_classes = useCommonStyles();
@@ -31,55 +69,97 @@ const ProjectsDraftsGrid = ({ props, state, profile, projects }) => {
   
 
   const {t} = useTranslation();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Paper className={classes.profileLowerStyle}>
-    <Typography
-      gutterBottom
-      component="h2"
-      variant="h6"
-      color="textPrimary"
-      className={classes.titleStyle}
-    >
-      {t('profile.projects.label')}
-      <CustomButton
-        className={clsx(classes.floatRight)}
-        variant="outlined"
-        margin="normal"
-        secondaryButtonStyle
-        onClick={() =>
-          props.history.push(
-            `/creators/${profile.username}/projects`,
-          )
-        }
-      >
-        {t('profile.projects.viewAll')}
-      </CustomButton>
-    </Typography>
-    <Grid container>
-      {Array.isArray(projects) &&
-        projects.map(project => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={6}
-            className={classes.projectGridStyle}
-            align="center"
-          >
-            <Project
-              project={project}
-              key={project.id}
-              updateProjects={res =>
-                updateProjects(res, state, props, toast)
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Projects" {...a11yProps(0)} />
+          <Tab label="Drafts" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <CustomButton
+          className={clsx(classes.viewAllBtn)}
+          variant="outlined"
+          margin="normal"
+          secondaryButtonStyle
+          onClick={() =>
+            props.history.push(
+              `/creators/${profile.username}/projects`,
+            )
+          }
+        >
+          {t('profile.projects.viewAll')}
+        </CustomButton>
+        <Grid container>
+          {Array.isArray(projects) &&
+            projects.map(project => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={6}
+                className={classes.gridStyle}
+                align="center"
+              >
+                <Project
+                  project={project}
+                  key={project.id}
+                  updateProjects={res =>
+                    updateProjects(res, state, props, toast)
                 
-              }
-              {...props}
-            />
-          </Grid>
-        ))}
-    </Grid>
-  </Paper>
+                  }
+                  {...props}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <CustomButton
+          className={clsx(classes.viewAllBtn)}
+          variant="outlined"
+          margin="normal"
+          secondaryButtonStyle
+          onClick={() =>
+            props.history.push(
+              `/creators/${profile.username}/drafts`,
+            )
+          }
+        >
+          {t('profile.projects.viewAll')}
+        </CustomButton>
+        <Grid container>
+          {Array.isArray(projects) &&
+            projects.map(project => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={6}
+                className={classes.gridStyle}
+                align="center"
+              >
+                <Project
+                  project={project}
+                  key={project.id}
+                  updateProjects={res =>
+                    updateProjects(res, state, props, toast)
+                
+                  }
+                  {...props}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </TabPanel>
+    </Paper>
   );
 }
 
