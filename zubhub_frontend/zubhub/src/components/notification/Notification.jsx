85457dd685d8d1e86f1bb1ec8ab1f410087eae7d
saxytony <anthony.cruzmacedo@protonmail.com>
2022-03-23 23:18:10 -0500

    Include i18next translation for timestamps

--- zubhub_frontend/zubhub/src/components/notification/Notification.jsx
+++ zubhub_frontend/zubhub/src/components/notification/Notification.jsx
@@ -3,15 +3,23 @@ import styles from '../../assets/js/styles/components/notification/NotificationS
 import { makeStyles } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 import API from '../../api';
-import { renderTimeAgo, getNotification } from './NotificationScripts';
+import { ListItem, ListItemAvatar, Avatar } from '@material-ui/core';
+import { ListItemText } from '@material-ui/core';
+import { dFormatter } from '../../assets/js/utils/scripts';
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles(styles);
 const Notification = ({ notification }) => {
   const classes = useStyles();
   const token = useSelector(store => store.auth.token);
+  const { t } = useTranslation();
+  const st = dFormatter(JSON.parse('"' + notification.time + '"'));
   return (
     <>
-      <div
+      <ListItem
+        alignItems="center"
+        disableGutters
+        button
         className={classes.notificationStyle}
         onClick={() => {
           if (!notification.viewed) {
@@ -23,19 +31,17 @@ const Notification = ({ notification }) => {
           window.location.href = notification.link;
         }}
       >
-        <img
-          className={classes.image}
-          src={notification.source.avatar}
-          alt="user-profile"
+        <ListItemAvatar>
+          <Avatar className={classes.image} src={notification.source.avatar} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={notification.message}
+          secondary={t(st.value + ' ' + st.key + ' ' + 'ago')}
+          style={{ color: 'black', width: '275px' }}
         />
-
-        <div className={classes.text}>
-          <p className={classes.message}>{getNotification(notification)}</p>
-          <p className={classes.time}>{renderTimeAgo(notification)}</p>
-        </div>
         {!notification.viewed && <div className={classes.viewDot}></div>}
         {notification.viewed && <div className={classes.unviewed}></div>}
-      </div>
+      </ListItem>
     </>
   );
 };
