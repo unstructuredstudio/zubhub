0637676d5b430d0cefe7d321d15b27da2d252e88
Muhammed Ali <muhamzyali@gmail.com>
2022-04-17 07:53:14 +0100

    minor changes to tests

--- zubhub_backend/zubhub/creators/tests/test_views.py
+++ zubhub_backend/zubhub/creators/tests/test_views.py
@@ -11,51 +11,48 @@ creator = get_user_model()
 
 
 class FetchAuthUserApiTests(TestCase):
-    """Test the users API (public)"""
+    """Test the users API"""
 
     def setUp(self):
         self.client = APIClient()
-        self.location = Location.objects.create(
-            name='Nigeria',
-        )
-        self.payload= {
-    "username": "test_dummy",
-    "email": "test_dummy@gmail.com",
-    "password1": "qwertyu12345",
-    "password2": "qwertyu12345",
-    "phone": "+57778899066",
-    "dateOfBirth": "1998-02-20",
-    "location":"Nigeria",
-    "bio": "ddd",
-    "subscribe": True
+        self.location = Location.objects.create(name="Nigeria")
+        self.payload = {
+            "username": "test_dummy",
+            "email": "test_dummy@gmail.com",
+            "password1": "qwertyu12345",
+            "password2": "qwertyu12345",
+            "phone": "+57778899066",
+            "dateOfBirth": "1998-02-20",
+            "location": "Nigeria",
+            "bio": "ddd",
+            "subscribe": True,
         }
 
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
-        payload_false = {
-            'username': 'bean',
-            'password': 'boom12345'
-        }
-        res = self.client.post(reverse('creators:signup_creator'), self.payload)
-        res_false = self.client.post(reverse('creators:signup_creator'), payload_false)
+        payload_false = {"username": "bean", "password": "boom12345"}
+        res = self.client.post(reverse("creators:signup_creator"), self.payload)
+        res_false = self.client.post(reverse("creators:signup_creator"), payload_false)
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         self.assertEqual(res_false.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertTrue(res) 
+        self.assertTrue(res)
+
+    def test_user_create_unsuccess(self):
+        """Test creating user with invalid payload is unsuccessful"""
+        payload_false = {"username": "bean", "password": "boom12345"}
+        res_false = self.client.post(reverse("creators:signup_creator"), payload_false)
+        self.assertEqual(res_false.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_user_for_profile_retrieval(self):
         """Test that you can retrieve user profile with given username"""
-        res = self.client.post(reverse('creators:signup_creator'), self.payload)
-        url = self.client.get(('/api/creators/test_dummy/'))
+        res = self.client.post(reverse("creators:signup_creator"), self.payload)
+        url = self.client.get(("/api/creators/test_dummy/"))
         self.assertEqual(url.status_code, status.HTTP_200_OK)
-    
+
     def test_fetch_authenticated_user(self):
         """ Test Fetching of profile of the authenticated user. """
-        create_user = self.client.post(reverse('creators:signup_creator'), self.payload)
-        url = self.client.get(reverse('creators:auth_user_detail'))
+        create_user = self.client.post(reverse("creators:signup_creator"), self.payload)
+        url = self.client.get(reverse("creators:auth_user_detail"))
         self.assertEqual(url.status_code, status.HTTP_200_OK)
-        new_user = creator.objects.get(username='test_dummy')
-        self.assertTrue(new_user) 
-
-
-
-
+        new_user_exists = creator.objects.filter(username="test_dummy").exists()
+        self.assertTrue(new_user_exists)
