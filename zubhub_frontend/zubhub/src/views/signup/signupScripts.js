982c20543f97bc4021d6139bb1918a8c65e8130c
alicendeh <alicendeh16@gmail.com>
2022-04-18 17:02:03 +0100

    Fixed issue209 Error Notification on Sign Up gets hidden on mobile UX

--- zubhub_frontend/zubhub/src/views/signup/signupScripts.js
+++ zubhub_frontend/zubhub/src/views/signup/signupScripts.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import countryMap from '../../assets/js/countryMap.json';
 import intlTelInput from 'intl-tel-input';
 import { calculateLabelWidth } from '../../assets/js/utils/scripts';
+import { toast } from 'react-toastify';
 
 /**
  * @constant vars
@@ -91,6 +92,10 @@ export const signup = (e, props) => {
             });
           }
         });
+    } else {
+      toast.error(
+        'Looks like there are a few errors in the registration form. Review it before clicking Signup!',
+      );
     }
   });
 };
