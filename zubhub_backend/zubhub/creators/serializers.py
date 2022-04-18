029252fb6fdfb7d1b269a4695958082ac5ec9b24
Andrew Lester <alester220@gmail.com>
2022-04-18 17:58:02 -0500

    Add search autocomplete (#393)
    * Update lockfile version without breaking npm ci
    
    * Fix search styling
    
    * Add search autocomplete using trigram similarity test
    
    * Use proper api function in frontend
    
    * Revert formatting on 2 unused files
    
    * Fix autocomplete default value
    
    * Expand clickable range for autocomplete links
    
    * Add tag GinIndex
    
    * Leave old tag search endpoint
    
    * Add autocompletes for creators and projects
    
    * Fix on click to search
    
    * Fix clicks
    
    * Autocomplete tags
    
    * Limit autocomplete results
    
    Co-authored-by: Aditya Jain <adityaj5@illinois.edu>

--- zubhub_backend/zubhub/creators/serializers.py
+++ zubhub_backend/zubhub/creators/serializers.py
@@ -13,21 +13,24 @@ from projects.utils import parse_comment_trees
 
 Creator = get_user_model()
 
+
 class CreatorMinimalSerializer(serializers.ModelSerializer):
-    followers = serializers.SlugRelatedField(
-        slug_field="id", read_only=True, many=True)
+    followers = serializers.SlugRelatedField(slug_field="id",
+                                             read_only=True,
+                                             many=True)
     comments = serializers.SerializerMethodField('get_profile_comments')
     projects_count = serializers.SerializerMethodField()
     following_count = serializers.IntegerField(read_only=True)
     members_count = serializers.SerializerMethodField('get_members_count')
-    tags = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    tags = serializers.SlugRelatedField(slug_field="name",
+                                        read_only=True,
+                                        many=True)
 
     class Meta:
         model = Creator
 
-        fields = ('id', 'username', 'avatar', 'comments', 'bio', 
-                    'followers', 'following_count', 'projects_count', 
-                    'members_count', 'tags')
+        fields = ('id', 'username', 'avatar', 'comments', 'bio', 'followers',
+                  'following_count', 'projects_count', 'members_count', 'tags')
 
     read_only_fields = ["id", "projects_count", "following_count", "tags"]
 
@@ -63,7 +66,7 @@ class CreatorMinimalSerializer(serializers.ModelSerializer):
 
         root_comments = list(
             map(lambda x: Comment.dump_bulk(x)[0], root_comments))
-        
+
         user = self.context.get("request").user
         return parse_comment_trees(user, root_comments, creators_dict)
 
@@ -72,53 +75,58 @@ class CreatorSerializer(CreatorMinimalSerializer):
     phone = serializers.CharField(allow_blank=True, default="")
     email = serializers.EmailField(allow_blank=True, default="")
     dateOfBirth = serializers.DateField(read_only=True)
-    location = serializers.SlugRelatedField(
-        slug_field='name', queryset=Location.objects.all())
-    tags = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    location = serializers.SlugRelatedField(slug_field='name',
+                                            queryset=Location.objects.all())
+    tags = serializers.SlugRelatedField(slug_field="name",
+                                        read_only=True,
+                                        many=True)
 
     class Meta:
         model = Creator
 
-        fields = ('id', 'username', 'email', 'phone', 'avatar', 'location', 'comments',
-                  'dateOfBirth', 'bio', 'followers', 'following_count', 'projects_count', 'members_count', 'tags')
+        fields = ('id', 'username', 'email', 'phone', 'avatar', 'location',
+                  'comments', 'dateOfBirth', 'bio', 'followers',
+                  'following_count', 'projects_count', 'members_count', 'tags')
 
-    read_only_fields = ["id", "projects_count",
-                        "following_count", "dateOfBirth", "tags"]
+    read_only_fields = [
+        "id", "projects_count", "following_count", "dateOfBirth", "tags"
+    ]
 
     def validate_email(self, email):
 
-        if(len(email) == 0 and len(self.initial_data.get("phone", "")) == 0):
+        if (len(email) == 0 and len(self.initial_data.get("phone", "")) == 0):
             raise serializers.ValidationError(
                 _("you must provide either email or phone number"))
 
-        if(Creator.objects.filter(email=email).count() > 0 and email != ""):
+        if (Creator.objects.filter(email=email).count() > 0 and email != ""):
             if email == self.context.get("request").user.email:
                 return email
             raise serializers.ValidationError(
                 _("a user with that email address already exists"))
 
-        if(self.context.get("request").user.email):
+        if (self.context.get("request").user.email):
             raise serializers.ValidationError(
                 _("to edit this field mail hello@unstructured.studio"))
 
         return email
 
     def validate_phone(self, phone):
-        if(len(phone) == 0 and len(self.initial_data.get("email", "")) == 0):
+        if (len(phone) == 0 and len(self.initial_data.get("email", "")) == 0):
             raise serializers.ValidationError(
                 _("you must provide either email or phone number"))
 
         if re.search(r'^\+\d{9,15}$', phone) == None and phone != "":
             raise serializers.ValidationError(
-                _("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
+                _("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+                  ))
 
-        if(Creator.objects.filter(phone=phone).count() > 0 and phone != ""):
+        if (Creator.objects.filter(phone=phone).count() > 0 and phone != ""):
             if phone == self.context.get("request").user.phone:
                 return phone
             raise serializers.ValidationError(
                 _("a user with that phone number already exists"))
 
-        if(self.context.get("request").user.phone):
+        if (self.context.get("request").user.phone):
             raise serializers.ValidationError(
                 _("to edit this field mail hello@unstructured.studio"))
 
@@ -129,64 +137,85 @@ class CreatorSerializer(CreatorMinimalSerializer):
         phone_number = PhoneNumber.objects.filter(user=creator)
         email_address = EmailAddress.objects.filter(user=creator)
 
-        if(len(phone_number) < 1):
+        if (len(phone_number) < 1):
             setup_user_phone(creator)
 
-        if(len(email_address) < 1):
+        if (len(email_address) < 1):
             setup_user_email(self.context.get("request"), creator, [])
 
         return creator
 
 
+class CreatorListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Creator
+
+        fields = [
+            "id",
+            "avatar",
+            "phone",
+            "username",
+            "bio",
+            "followers",
+            "followers_count",
+            "following_count",
+            "projects_count",
+            "tags",
+        ]
+
+
 class LocationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Location
-        fields = ('name',)
+        fields = ('name', )
 
 
 class CustomRegisterSerializer(RegisterSerializer):
     phone = serializers.CharField(allow_blank=True, default="")
     email = serializers.EmailField(allow_blank=True, default="")
     dateOfBirth = serializers.DateField()
-    location = serializers.SlugRelatedField(
-        slug_field='name', queryset=Location.objects.all())
+    location = serializers.SlugRelatedField(slug_field='name',
+                                            queryset=Location.objects.all())
     bio = serializers.CharField(allow_blank=True, default="", max_length=255)
     subscribe = serializers.BooleanField(default=False)
 
     def validate_email(self, email):
-        if(len(email) == 0 and len(self.initial_data.get("phone", "")) == 0):
+        if (len(email) == 0 and len(self.initial_data.get("phone", "")) == 0):
             raise serializers.ValidationError(
                 _("you must provide either email or phone number"))
 
-        if(Creator.objects.filter(email=email).count() > 0 and email != ""):
+        if (Creator.objects.filter(email=email).count() > 0 and email != ""):
             raise serializers.ValidationError(
                 _("A user with that email address already exists"))
 
         return email
 
     def validate_phone(self, phone):
-        if(len(phone) == 0 and len(self.initial_data.get("email", "")) == 0):
+        if (len(phone) == 0 and len(self.initial_data.get("email", "")) == 0):
             raise serializers.ValidationError(
                 _("you must provide either email or phone number"))
 
         if re.search(r'^\+\d{9,15}$', phone) == None and phone != "":
             raise serializers.ValidationError(
-                _("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
+                _("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+                  ))
 
-        if(Creator.objects.filter(phone=phone).count() > 0 and phone != ""):
+        if (Creator.objects.filter(phone=phone).count() > 0 and phone != ""):
             raise serializers.ValidationError(
                 _("A user with that phone number already exists"))
 
         return phone
 
     def validate_dateOfBirth(self, dateOfBirth):
-        if((date.today() - dateOfBirth).days < 0):
+        if ((date.today() - dateOfBirth).days < 0):
             raise serializers.ValidationError(
                 _("Date of Birth must be less than today's date"))
         return dateOfBirth
 
     def validate_location(self, location):
-        if(len(location.name) < 1):
+        if (len(location.name) < 1):
             raise serializers.ValidationError(_("Location is required"))
         return location
 
@@ -216,17 +245,20 @@ class ConfirmGroupInviteSerializer(serializers.Serializer):
 
 class AddGroupMembersSerializer(serializers.Serializer):
     group_members = serializers.JSONField(required=False, allow_null=True)
-    csv = serializers.FileField(
-        required=False, allow_null=True, allow_empty_file=True)
+    csv = serializers.FileField(required=False,
+                                allow_null=True,
+                                allow_empty_file=True)
 
     def validate_group_members(self, group_members):
-        if(len(group_members) == 0 and not self.initial_data.get("csv")):
+        if (len(group_members) == 0 and not self.initial_data.get("csv")):
             raise serializers.ValidationError(
-                _("you must submit group member usernames either through the form or as csv"))
+                _("you must submit group member usernames either through the form or as csv"
+                  ))
         return group_members
 
     def validate_csv(self, csv):
-        if(not csv and len(self.initial_data.get("group_members")) == 0):
+        if (not csv and len(self.initial_data.get("group_members")) == 0):
             raise serializers.ValidationError(
-                _("you must submit group member usernames either through the form or as csv"))
+                _("you must submit group member usernames either through the form or as csv"
+                  ))
         return csv
