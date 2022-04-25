605a2485062445c0599f440a71b49bec48c5924b
Muhammed Ali <muhamzyali@gmail.com>
2022-04-25 15:03:14 +0100

    update comment to create and update API

--- zubhub_backend/zubhub/projects/views.py
+++ zubhub_backend/zubhub/projects/views.py
@@ -39,7 +39,8 @@ class ProjectCreateAPIView(CreateAPIView):
             ],\n
             "video": "string",\n
             "materials_used": "string",\n
-            "category": "string"\n
+            "category": "string",\n
+            "publish": {"type": 4, "visible_to": []}\n
         }\n
     """
     queryset = Project.objects.all()
@@ -71,7 +72,8 @@ class ProjectUpdateAPIView(UpdateAPIView):
             ],\n
             "video": "string",\n
             "materials_used": "string",\n
-            "category": "string"\n
+            "category": "string",\n
+            "publish": {"type": 4, "visible_to": []}\n
         }\n
     """
     queryset = Project.objects.all()
