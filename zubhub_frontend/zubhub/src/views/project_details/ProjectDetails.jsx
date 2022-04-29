d14256f192d36b6d4940552e8ada62da19642f61
Zora Zhang <zorazrr@gmail.com>
2022-04-29 17:35:59 -0500

    bottom bar working

--- zubhub_frontend/zubhub/src/views/project_details/ProjectDetails.jsx
+++ zubhub_frontend/zubhub/src/views/project_details/ProjectDetails.jsx
@@ -276,7 +276,7 @@ function ProjectDetails(props) {
                   className={classes.bottomBarWrapperStyle}
                 >
                   {/* Claps, Bookmarks, and Views Bar */}
-                  <Grid xs={12} className={classes.actionGrid}>
+                  <Grid xs={mediaQuery ? 12 : 6} className={classes.actionGrid}>
                     <div
                       className={
                         mediaQuery
@@ -371,7 +371,7 @@ function ProjectDetails(props) {
                   </Grid>
                   {/* Claps, Bookmarks, and Views Bar */}
                   {/* User Info Grid */}
-                  <Grid xs={12} className={classes.userGrid}>
+                  <Grid xs={mediaQuery ? 12 : 6} className={classes.userGrid}>
                     <Grid container>
                       <Grid item className={classes.creatorProfileStyle}>
                         <Link
