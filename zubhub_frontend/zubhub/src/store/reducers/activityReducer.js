7c4f1b95ef333963e9f8088e3c65f18e7b4c1003
souad988 <59707859+souad988@users.noreply.github.com>
2022-06-16 14:06:25 +0100

    Create activity reducer with initial state for all activities

---
+++ zubhub_frontend/zubhub/src/store/reducers/activityReducer.js
@@ -0,0 +1,18 @@
+const default_state = {
+    all_activities: {},
+  };
+  
+  const activities = (state = default_state, action) => {
+    switch (action.type) {
+      case 'SET_ACTIVITIES':
+        return {
+          ...state,
+          ...action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default activities;
+  
