3716554e811df798d12ba13bd512d7ff377c1de8
souad988 <59707859+souad988@users.noreply.github.com>
2022-08-19 08:11:40 +0100

    Fix styled-components package import issue

--- zubhub_frontend/zubhub/src/views/activities/activities.jsx
+++ zubhub_frontend/zubhub/src/views/activities/activities.jsx
@@ -1,99 +1,96 @@
-import React,{useEffect} from 'react'
-import {connect} from 'react-redux'
-import {getActivities} from '../../store/actions/activityActions'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getActivities } from '../../store/actions/activityActions';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Activity from '../../components/activity/activity'
+import Activity from '../../components/activity/activity';
 import styles, {
   sliderSettings,
 } from '../../assets/js/styles/views/activities/activitiesStyles';
 import { makeStyles } from '@material-ui/core/styles';
-import styled from "styled-components"
+import styled from 'styled-components';
 import { Grid, Box } from '@material-ui/core';
 import { useState } from 'react';
 const useStyles = makeStyles(styles);
 
- const StyledSlider = styled(Slider)
-   `
+const StyledSlider = styled(Slider)`
   .slick-next {
-    top: 16vh!important;
-  } 
+    top: 16vh !important;
+  }
   .slick-prev {
-    top: 16vh!important;
+    top: 16vh !important;
   }
   .slick-list {
     padding: 0 !important;
   }
   .slick-slider {
     padding-top: 0 !important;
-    maxHeight: 40vh;
+    maxheight: 40vh;
   }
   .slick-slide {
     margin: 0 5px;
   }
- `;
+`;
 
 function Activities(props) {
-   const classes = useStyles();
-   const activities = props.activities.all_activities
-   const [activeCarouselIndex,setActiveCarouselIndex] = useState(0)
-   useEffect(() => {
-     props.getActivities()
-   }, [])
-  const activeSlide = (currentSlide) =>{
-    setActiveCarouselIndex(currentSlide)
-  } 
+  const classes = useStyles();
+  const activities = props.activities.all_activities;
+  const [activeCarouselIndex, setActiveCarouselIndex] = useState(0);
+  useEffect(() => {
+    props.getActivities();
+  }, []);
+  const activeSlide = currentSlide => {
+    setActiveCarouselIndex(currentSlide);
+  };
 
   return (
-        <div>
-          <div className={classes.bannerContainer}>
-            <Box className={classes.sliderBox}>
-             <StyledSlider {...sliderSettings(3,activeSlide)} >
-               {activities? 
-               activities.map((activity,index) => (
-                  index === activeCarouselIndex ?  
-                  <div className={classes.activeSlideContainer}>
-                    <iframe
-                          id={activity.id}
-                          className={classes.activeCarouselVideo}
-                          src={activity.video+`?autoplay=1&mute=1`}
-                          allow='autoplay; encrypted-media'
-                        /> 
-                        </div>: 
-                    <div className={classes.slideContainer}>    
-                    <iframe
-                          id={activity.id}
-                          className={classes.sliderVideo}
-                          src={activity.video}
-                        />
-                    </div>      
-               ))
-              :'No activities created yet!!'}
-             </StyledSlider>
-             </Box> 
-            </div>
+    <div>
+      <div className={classes.bannerContainer}>
+        <Box className={classes.sliderBox}>
+          <StyledSlider {...sliderSettings(3, activeSlide)}>
+            {activities
+              ? activities.map((activity, index) =>
+                  index === activeCarouselIndex ? (
+                    <div className={classes.activeSlideContainer}>
+                      <iframe
+                        id={activity.id}
+                        className={classes.activeCarouselVideo}
+                        src={activity.video + `?autoplay=1&mute=1`}
+                        allow="autoplay; encrypted-media"
+                      />
+                    </div>
+                  ) : (
+                    <div className={classes.slideContainer}>
+                      <iframe
+                        id={activity.id}
+                        className={classes.sliderVideo}
+                        src={activity.video}
+                      />
+                    </div>
+                  ),
+                )
+              : 'No activities created yet!!'}
+          </StyledSlider>
+        </Box>
+      </div>
 
-              <Grid container
-              className={classes.activityListContainer}
-              >
-              {activities.map(activity => (
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  md={6}
-                  align="center"
-                  className= {classes.activityBoxContainer}
-                >
-                  <Activity
-                    activity={activity}
-                  />
-                </Grid>
-              ))}
-             </Grid>
-        </div>
-  )
+      <Grid container className={classes.activityListContainer}>
+        {activities.map(activity => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            align="center"
+            className={classes.activityBoxContainer}
+          >
+            <Activity activity={activity} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
@@ -106,7 +103,7 @@ const mapDispatchToProps = dispatch => {
   return {
     getActivities: () => {
       return dispatch(getActivities());
-    }
+    },
   };
 };
 
