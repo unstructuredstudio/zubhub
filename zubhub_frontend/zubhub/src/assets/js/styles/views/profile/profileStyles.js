4aa7cc1f7120106977c5527639ac091dccb3c7ab
Deepanshu Gautam <92849065+Deepanshu039@users.noreply.github.com>
2022-09-03 06:31:05 +0530

    User stats enhancement (#505)
    * Use same border radius for next and prev buttons
    
    * refactor button border radius code
    
    * reverted master
    
    * user's profile header refactor

--- zubhub_frontend/zubhub/src/assets/js/styles/views/profile/profileStyles.js
+++ zubhub_frontend/zubhub/src/assets/js/styles/views/profile/profileStyles.js
@@ -4,47 +4,63 @@ const styles = theme => ({
   root: {
     flex: '1 0 auto',
   },
-  profileHeaderStyle: {
-    paddingTop: '1.5em',
-    background: 'rgba(255,204,0,1)',
-    background:
-      'linear-gradient(to bottom, rgba(255,204,0,1) 0%, rgba(255,229,133,1) 25%, rgba(255,255,255,1) 61%, rgba(255,255,255,1) 100%)',
-    [theme.breakpoints.down('511')]: {
-      paddingTop: '4em',
+  classFlex: {
+    display: 'flex',
+    [theme.breakpoints.down('657')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
     },
   },
+  profileHeaderStyle: {
+    paddingTop: '1.5em',
+    paddingBottom: '1.5em',
+    background: 'rgba(255,204,0,1)',
+    background:
+    'linear-gradient(to bottom, rgba(255,204,0,1) 0%, rgba(255,229,133,1) 25%, rgba(255,255,255,1) 61%, rgba(255,255,255,1) 100%)',
+  },
   avatarBoxStyle: {
-    width: '100%',
+    minWidth: '35%',
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   profileShareButtonStyle: {
     borderRadius: '50% !important',
   },
   avatarStyle: {
-    width: '100%',
-    height: '100%',
-    paddingTop: '1.5em',
-    paddingBottom: '1.5em',
+    width: '80%',
+    height: '80%',
+    paddingLeft: '1em',
+    paddingRight: '1em',
     '& img': {
-      width: '10em',
+      width: '8em',
       backgroundColor: 'white',
-      height: '10em',
+      height: '8em',
       borderRadius: '50%',
       boxShadow: `0 3px 5px 2px rgba(0, 0, 0, .12)`,
     },
   },
   ProfileDetailStyle: {
-    width: '100%',
+    minWidth: '30%',
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'start',
+    justifyContent: 'center',
+    marginRight: '1.5em',
+    [theme.breakpoints.down('657')]: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: '1.5em'
+    },
   },
   userNameStyle: {
     fontWeight: 900,
     fontSize: '2rem',
     position: 'relative',
-    [theme.breakpoints.down('470')]: {
+    overflowWrap: 'anywhere',
+    
+    [theme.breakpoints.down('750')]: {
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'center',
@@ -54,23 +70,49 @@ const styles = theme => ({
   tagsContainerStyle: {
     display: 'flex',
     flexWrap: 'wrap',
+    paddingTop: '0.5em',
+    paddingBottom: '0.5em',
+    [theme.breakpoints.down('657')]: {
+      paddingTop: '0',
+      paddingBottom: '0',
+    },
   },
-  emailStyle: { marginBottom: '0.5em' },
+  removeTagMargin: {
+    marginLeft: '0 !important',
+  },
+  centerTag: {
+  alignItems: 'center',
+  },
+  emailStyle: { 
+    overflowWrap: 'anywhere',
+  },
   dividerStyle: {
-    width: '100vw',
   },
   moreInfoBoxStyle: {
-    height: '3em',
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    alignItems: 'end',
+    flexWrap: 'wrap',
+    [theme.breakpoints.down('657')]: {
+      marginTop: '1.3em',
+    },
   },
   moreInfoStyle: {
-    marginLeft: '0.5em',
-    marginRight: '0.5em',
-    fontWeight: 'bold',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  moreInfoStyle1: {
+    fontWeight: '500',
     fontSize: '0.9rem',
+    color: '#00B8C4',
+    paddingLeft: '5px',
+    paddingRight: '5px',
+  },
+  moreInfoStyle2: {
+    fontWeight: '550',
+    fontSize: '3.5em',
     color: '#00B8C4',
   },
   profileLowerStyle: {
@@ -128,7 +170,21 @@ const styles = theme => ({
   textDecorationNone: {
     textDecoration: 'none',
   },
-  floatRight: { float: 'right' },
+  floatRight: 
+  { 
+    float: 'right',
+  },
+  secondaryButtonMargin: 
+  { 
+    marginTop: '1em',
+  },
+  verticalOption: 
+  { 
+    float: 'right' ,
+    [theme.breakpoints.down('657')]: {
+      float: 'none',
+    },
+  },
   displayNone: { display: 'none' },
   largeLabel: {
     fontSize: '1.3rem',
