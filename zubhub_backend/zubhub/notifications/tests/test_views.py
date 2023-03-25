e957e39d7a9127ab7632c1616a669a722af3ab26
Hemant Kumar Singh <iammrhks@yahoo.com>
2023-03-26 01:16:57 +0530

    ask for help

--- zubhub_backend/zubhub/notifications/tests/test_views.py
+++ zubhub_backend/zubhub/notifications/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse, reverse_lazy
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIClient
+from django.test import TestCase
 from notifications.models import Notification
 from creators.models import Creator
 from notifications.serializers import NotificationSerializer
@@ -8,9 +9,54 @@ from django.utils import timezone
 import json
 from django.test import Client
 from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.contrib.auth import get_user_model
+
+creator = get_user_model()
+
+class UserNotificationsAPIViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        self.first_creator = Creator.objects.create(username="krishna")
+        self.second_creator = Creator.objects.create(username="kali")
+        self.client = APIClient()
+
+        self.payload = Notification.objects.create(
+            link="https://zubhub.com/exampleclap",
+            type=Notification.Type.CLAP,
+            recipient=self.first_creator,
+            message="Clap",
+            viewed=False
+        )
+        self.payload.save()
+        self.payload.sources.add(self.second_creator, self.second_creator)
+        self.payload.save()
+
+        self.auth = {
+            "username": "test_dummy2",
+            "email": "test_dummy2@gmail.com",
+            "password1": "qwertyu12345",
+            "password2": "qwertyu12345",
+            "phone": "+57778899066",
+            "dateOfBirth": "1998-02-20",
+            "location": "Nigeria",
+            "bio": "ddd",
+            "subscribe": True,
+        }
+
+    def test_get_user_notifications(self):
+        rgst = self.client.post(reverse("creators:signup_creator"), self.auth, format="json")
+        res = self.client.get(reverse('notifications:user-notifications'))
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data['count'], 1)
+
+    def test_get_user_notifications_unauthorized(self):
+        res = self.client.get(reverse('notifications:user-notifications'))
+        self.assertEqual(res.status_code, 403)
 
 
-class MarkNotificationAsViewedAPIViewTest(APITestCase):
+class MarkNotificationAsViewedAPIViewTest(TestCase):
     @classmethod
     def setUpTestData(self):
         self.first_creator = Creator.objects.create(username="krishna")
@@ -34,8 +80,13 @@ class MarkNotificationAsViewedAPIViewTest(APITestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.data['viewed'], True)
 
+    def test_mark_notification_as_unauthozired(self):
+        self.payload= Notification.objects.get(id=self.payload.id)
+        res = self.client.put("/api/notifications/{}/update".format(self.payload.id), data=self.payload, content_type="application/json", follow=True)
+        self.assertEqual(res.status_code, 403)
 
-class DeleteNotificationAPIViewTest(APITestCase):
+
+class DeleteNotificationAPIViewTest(TestCase):
     @classmethod
     def setUpTestData(self):
         self.first_creator = Creator.objects.create(username="krishna")
@@ -53,29 +104,9 @@ class DeleteNotificationAPIViewTest(APITestCase):
         self.payload.sources.add(self.second_creator, self.second_creator)
         self.payload.save()
 
-    def test_delete_notification(self):
-        res = self.client.delete("/api/notifications/{}/delete".format(self.payload.id), data=self.payload, content_type="application/json", follow=True)
-        self.assertEqual(res.status_code, 204)
-
-
-class UserNotificationsAPIViewTest(APITestCase):
-
-    @classmethod
-    def setUpTestData(self):
-        self.first_creator = Creator.objects.create(username="krishna")
-        self.second_creator = Creator.objects.create(username="kali")
-        self.client = APIClient()
-
-        self.payload = Notification.objects.create(
-            link="https://zubhub.com/exampleclap",
-            type=Notification.Type.CLAP,
-            recipient=self.first_creator,
-            message="Clap",
-            viewed=False
-        )
-        self.x = {
-            "username": "test_dummy",
-            "email": "test_dummy@gmail.com",
+        self.auth = {
+            "username": "test_dummy2",
+            "email": "test_dummy2@gmail.com",
             "password1": "qwertyu12345",
             "password2": "qwertyu12345",
             "phone": "+57778899066",
@@ -84,19 +115,12 @@ class UserNotificationsAPIViewTest(APITestCase):
             "bio": "ddd",
             "subscribe": True,
         }
-        self.payload.save()
-        self.payload.sources.add(self.second_creator, self.second_creator)
-        self.payload.save()
 
-        self.user = Creator.objects.create(
-            username= "test_dummy",
-            email= "test_dummy@gmail.com",
-            password= "qwertyu12345",
-        )
-        token, created = Token.objects.get_or_create(user=self.user)
-        self.client = Client(HTTP_AUTHORIZATION='Token ' + self.user.key)
-        
-    def test_get_user_notifications(self):
-        res = self.client.get(reverse('notifications:user-notifications'))
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.data['count'], 1)
+    def test_delete_notification(self):
+        rgst = self.client.post(reverse("creators:signup_creator"), self.auth, format="json")
+        res = self.client.delete(f"/api/notifications/{self.payload.id}/delete", data=self.payload, content_type="application/json", follow=True)
+        self.assertEqual(res.status_code, 204)
+
+    def test_delete_notification_unauthorized(self):
+        res = self.client.delete("/api/notifications/{}/delete".format(self.payload.id), data=self.payload, content_type="application/json", follow=True)
+        self.assertEqual(res.status_code, 403)
