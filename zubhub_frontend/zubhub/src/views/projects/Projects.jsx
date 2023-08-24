30a64fa8e924782b6fc04a831010fc433031d030
Hemant Kumar Singh <iammrhks@yahoo.com>
2023-08-24 11:08:47 +0530

    call for testing

--- zubhub_frontend/zubhub/src/views/projects/Projects.jsx
+++ zubhub_frontend/zubhub/src/views/projects/Projects.jsx
@@ -8,7 +8,9 @@ import CheckIcon from '@material-ui/icons/CheckCircle';
 import { toast } from 'react-toastify';
 import child from '../../assets/images/child.jpg';
 import teams from '../../assets/images/teams.jpg';
-import review from '../../assets/images/review.jpg';
+import review1 from '../../assets/images/review1.png';
+import review2 from '../../assets/images/review2.png';
+import review3 from '../../assets/images/review3.png';
 import global from '../../assets/images/global.jpg';
 import { makeStyles } from '@material-ui/core/styles';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
@@ -43,13 +45,34 @@ function Projects(props) {
   const classes = useStyles();
   const common_classes = useCommonStyles();
 
+  const [activeSlide, setActiveSlide] = React.useState(0);
+
+  const handleSlideChange = index => {
+    setActiveSlide(index);
+  };
+
   const [state, setState] = React.useState({
     loading: true,
+    isMobileView: false,
   });
 
   React.useEffect(() => {
     fetchStaffPicks(props);
     handleSetState(fetchPage(null, props));
+
+    const checkMobileView = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isMobileView: window.innerWidth < 500, // Adjust the breakpoint as needed
+      }));
+    };
+
+    checkMobileView(); // Initial check
+    window.addEventListener('resize', checkMobileView);
+
+    return () => {
+      window.removeEventListener('resize', checkMobileView);
+    };
   }, []);
 
   const handleSetState = obj => {
@@ -60,8 +83,12 @@ function Projects(props) {
     }
   };
 
-  const { loading } = state;
-  const { results: projects, previous: prev_page, next: next_page } = props.projects.all_projects;
+  const { loading, isMobileView } = state;
+  const {
+    results: projects,
+    previous: prev_page,
+    next: next_page,
+  } = props.projects.all_projects;
   const { hero } = props.projects;
   const staff_picks = props.projects.staff_picks;
   const { t } = props;
@@ -75,42 +102,52 @@ function Projects(props) {
           (
             <Box className={classes.root}>
               {hero && hero.id ? (
-                <Box className={classes.heroSectionStyle}>
-                  <Box className={classes.heroContainerStyle}>
-                    <Box className={classes.heroMessageContainerStyle}>
-                    <br/><br/><Typography className={classes.heroMessageSecondaryStyle}>
-                        {t('projects.1')}
-                      </Typography><br/>
-                      <Typography className={classes.heroMessageSecondaryStyle}>
-                        {t('projects.2')}
-                      </Typography><br/>
-                      <Typography className={classes.heroMessagePrimaryStyle}>
-                        {hero.title}
-                      </Typography><br/>
-                      <CustomButton
-                        className={classes.heroButtonStyle}
-                        size="small"
-                        primaryButtonStyle
-                        onClick={() => props.history.push('/projects/create')}
-                      >
-                        {t('projects.shareProject')}
-                      </CustomButton>
-                    </Box>
-                    <Box
-                      className={clsx(classes.heroImageContainerStyle, {
-                        [common_classes.displayNone]: !hero.activity_url,
-                      })}
-                    >
-                      
-                        <img
-                          className={classes.heroImageStyle}
-                          src={hero.image_url}
-                          alt=""
-                        />
-                    </Box>
-                  </Box>
-                </Box>
+                 <Box className={classes.heroSectionStyle}>
+                 <Box className={classes.heroContainerStyle}>
+                   {state.isMobileView && hero && hero.id && (
+                     <Box className={classes.heroImageContainerStyle}>
+                       <img
+                         className={classes.heroImageStyle}
+                         src={hero.image_url}
+                         alt=""
+                       />
+                     </Box>
+                   )}
+                   <Box className={classes.heroMessageContainerStyle}>
+                     <br />
+                     <Typography className={classes.heroMessageSecondaryStyle}>
+                       {t('projects.1')}
+                     </Typography>
+                     <br />
+                     <Typography className={classes.heroMessageSecondaryStyle}>
+                       {t('projects.2')}
+                     </Typography>
+                     <Typography className={classes.heroMessagePrimaryStyle}>
+                       {hero.title}
+                     </Typography>
+                     <br />
+                     <CustomButton
+                       className={classes.heroButtonStyle}
+                       size="small"
+                       primaryButtonStyle
+                       onClick={() => props.history.push('/projects/create')}
+                     >
+                       {t('projects.shareProject')}
+                     </CustomButton>
+                   </Box>
+                   {!state.isMobileView && hero && hero.id && (
+                     <Box className={classes.heroImageContainerStyle}> 
+                       <img
+                         className={classes.heroImageStyle}
+                         src={hero.image_url}
+                         alt=""
+                       />
+                     </Box>
+                   )}
+                 </Box>
+               </Box>
               ) : null}
+            
           <Box className={classes.mainContainerStyle}>
             {staff_picks &&
               staff_picks.map(staff_pick => (
@@ -217,7 +254,20 @@ function Projects(props) {
           
           <Box className={classes.SectionStyle}>
                   <Box className={classes.heroContainerStyle}>
-                    <Box className={classes.MessageContainerStyle}>
+                  {state.isMobileView ? '' :(
+                  <Box
+                      className={clsx(classes.ambassadorImageStyle, {
+                        [common_classes.displayNone]: !hero.activity_url,
+                      })}
+                      style={{ marginTop: state.isMobileView ? '0' : '2em' }}
+                    >
+                        <img
+                          className={classes.ambassadorImageStyle}
+                          src={child}
+                          alt=""
+                        />
+                    </Box>)}
+                    <Box className={classes.MessageContainerStyle} style={{ marginLeft: state.isMobileView ? '0' : '15em' }}>
                     <br/><Typography className={classes.MessagePrimaryStyle}>
                         {t('projects.child.title')}
                       </Typography><br/><br/>
@@ -239,7 +289,8 @@ function Projects(props) {
                         {t('projects.zubhubAmbassadors')}
                       </CustomButton>
                     </Box>
-                    <Box
+                    {state.isMobileView ? (
+                  <Box
                       className={clsx(classes.ambassadorImageStyle, {
                         [common_classes.displayNone]: !hero.activity_url,
                       })}
@@ -249,7 +300,7 @@ function Projects(props) {
                           src={child}
                           alt=""
                         />
-                    </Box>
+                    </Box>):''}
                   </Box>
                 </Box>
 
@@ -278,6 +329,7 @@ function Projects(props) {
                 className={clsx(classes.teamsImageContainerStyle, {
                   [common_classes.displayNone]: !hero.activity_url,
                 })}
+                style={{ marginTop: state.isMobileView ? '0' : '4em' }}
               >
                   <img
                     className={classes.teamsImageStyle}
@@ -288,70 +340,114 @@ function Projects(props) {
             </Box>
           </Box>
 
-          <Box className={clsx(classes.heroSectionStyle, classes.centerCarousel)}>
-          <div className={classes.carouselContainer}>
-            <Carousel showThumbs={false} infiniteLoop={true}showStatus={false}>
-            <div className={classes.carouselSlide}>
-                <Box className={classes.heroContainerStyle}>
-                  <Box className={classes.heroMessageContainerStyle}>
-                    <Typography className={classes.MessagePrimaryStyle}>
-                      {t('projects.reviews.title')}
-                    </Typography>
-                    <Typography className={classes.heroMessageSecondaryStyle}>
-                      {t('projects.reviews.review')}
-                    </Typography>
-                    <Typography className={classes.heroMessagePrimaryStyle}>
-                      {t('projects.reviews.name')}
-                    </Typography>
-                    <Typography className={classes.MessageSecondaryStyle}>
-                      {t('projects.reviews.designation')}
-                    </Typography>
-                  </Box>
-                  <Box
-                    className={clsx(classes.heroImageContainerStyle, {
-                      [common_classes.displayNone]: !hero.activity_url,
-                    })}
-                  >
+          <Box
+      className={clsx(classes.heroSectionStyle, classes.centerCarousel)}
+      style={{
+        backgroundColor: activeSlide === 1 ? '#00B8C4' : activeSlide === 2 ? '#292535' : '#DC3545', // Replace 'default-color' with your actual default background color
+      }}
+    >
+            <div className={classes.carouselContainer}>
+              <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} onChange={handleSlideChange}>
+                <div className={classes.carouselSlide}>
+                  <Box className={classes.heroContainerStyle}>
+                    <Box className={classes.heroMessageContainerStyle} style={{ textAlign: 'left' }}>
+                      <Typography
+                        className={classes.MessagePrimaryStyle}
+                      >
+                        {t('projects.reviews.title')}
+                      </Typography>
+                      <Typography className={classes.heroMessageSecondaryStyle}>
+                        {t('projects.reviews.1.review')}
+                      </Typography>
+                      <Typography className={classes.heroMessagePrimaryStyle}>
+                        {t('projects.reviews.1.name')}
+                      </Typography>
+                      <Typography className={classes.MessageSecondaryStyle}>
+                        {t('projects.reviews.1.designation')}
+                      </Typography>
+                    </Box>
+                    <Box
+                      className={clsx(classes.heroImageContainerStyle, {
+                        [common_classes.displayNone]: !hero.activity_url,
+                      })}
+                    >
                       <img
                         className={classes.reviewImageStyle}
-                        src={review}
+                        src={review1}
                         alt=""
+                        style={{ width: '200px', height: '200px' }}
                       />
+                    </Box>
                   </Box>
-                </Box>
-              </div>
-              <div className={classes.carouselSlide}>
+                </div>
+                <div className={classes.carouselSlide} style={{ backgroundColor: '#00B8C4' }}>  
                 <Box className={classes.heroContainerStyle}>
-                  <Box className={classes.heroMessageContainerStyle}>
-                    <Typography className={classes.heroMessagePrimaryStyle}>
-                      {t('projects.reviews.title')}
-                    </Typography>
-                    <Typography className={classes.heroMessageSecondaryStyle}>
-                      {t('projects.reviews.review')}
-                    </Typography>
-                    <Typography className={classes.heroMessagePrimaryStyle}>
-                      {t('projects.reviews.name')}
-                    </Typography>
-                    <Typography className={classes.MessageSecondaryStyle}>
-                      {t('projects.reviews.designation')}
-                    </Typography>
-                  </Box>
-                  <Box
-                    className={clsx(classes.heroImageContainerStyle, {
-                      [common_classes.displayNone]: !hero.activity_url,
-                    })}
-                  >
+                    <Box className={classes.heroMessageContainerStyle} style={{ textAlign: 'left' }}>
+                      <Typography
+                        className={classes.MessagePrimaryStyle}
+                      >
+                        {t('projects.reviews.title')}
+                      </Typography>
+                      <Typography className={classes.heroMessageSecondaryStyle}>
+                        {t('projects.reviews.2.review')}
+                      </Typography>
+                      <Typography className={classes.heroMessagePrimaryStyle}>
+                        {t('projects.reviews.2.name')}
+                      </Typography>
+                      <Typography className={classes.MessageSecondaryStyle}>
+                        {t('projects.reviews.2.designation')}
+                      </Typography>
+                    </Box>
+                    <Box
+                      className={clsx(classes.heroImageContainerStyle, {
+                        [common_classes.displayNone]: !hero.activity_url,
+                      })}
+                    >
                       <img
                         className={classes.reviewImageStyle}
-                        src={review}
+                        src={review2}
                         alt=""
+                        style={{ width: '200px', height: '200px' }}
                       />
-                  </Box>
-                </Box>
-              </div>
-            </Carousel>
+                    </Box>
+                  </Box>  
+                </div>
+                <div className={classes.carouselSlide} style={{ backgroundColor: '#292535' }}>  
+                <Box className={classes.heroContainerStyle}>
+                    <Box className={classes.heroMessageContainerStyle} style={{ textAlign: 'left' }}>
+                      <Typography
+                        className={classes.MessagePrimaryStyle}
+                      >
+                        {t('projects.reviews.title')}
+                      </Typography>
+                      <Typography className={classes.heroMessageSecondaryStyle}>
+                        {t('projects.reviews.3.review')}
+                      </Typography>
+                      <Typography className={classes.heroMessagePrimaryStyle}>
+                        {t('projects.reviews.3.name')}
+                      </Typography>
+                      <Typography className={classes.MessageSecondaryStyle}>
+                        {t('projects.reviews.3.designation')}
+                      </Typography>
+                    </Box>
+                    <Box
+                      className={clsx(classes.heroImageContainerStyle, {
+                        [common_classes.displayNone]: !hero.activity_url,
+                      })}
+                    >
+                      <img
+                        className={classes.reviewImageStyle}
+                        src={review3}
+                        alt=""
+                        style={{ width: '200px', height: '200px' }}
+                      />
+                    </Box>
+                  </Box>  
+                </div>
+              </Carousel>
             </div>
           </Box>
+
 
 
           <Box className={classes.SectionStyle}>
