4c5896accefc9984eafe9e26442e0b5161585afc
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2023-08-27 16:06:07 +0100

    Fix on Project card component

--- zubhub_frontend/zubhub/src/components/project/Project.jsx
+++ zubhub_frontend/zubhub/src/components/project/Project.jsx
@@ -96,7 +96,7 @@ function Project(props) {
               </Typography>
             </Box>
             <Link
-              style={{ flexGrow: 1 }}
+              style={{ marginTop: 'auto' }}
               to={`/creators/${project.creator.username}`}
               className={classes.textDecorationNone}
             >
