68c42d6b38808ae633f5d1a5d77d7d2fd884b935
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2023-09-05 07:45:07 +0100

    Fixed issue with images not showing in generated pdf

--- zubhub_frontend/zubhub/src/views/activity_details/ActivityDetailsV2.jsx
+++ zubhub_frontend/zubhub/src/views/activity_details/ActivityDetailsV2.jsx
@@ -100,7 +100,7 @@ export default function ActivityDetailsV2(props) {
           padding: 10,
           filename: `${activity.title}.pdf`,
           image: { type: 'jpeg', quality: 0.98 },
-          html2canvas: { scale: 2 },
+          html2canvas: { scale: 2, useCORS: true },
           jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
         };
 
