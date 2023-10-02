1b26d2af2018b4de2b2acaf62d24d59e5068dfc2
gift256 <giftnamulika@gmail.com>
2023-10-02 13:58:38 +0300

    add useIsInViewPort hook

---
+++ zubhub_frontend/zubhub/src/hooks/useIsInViewPort.hook.js
@@ -0,0 +1,24 @@
+import { useState, useMemo, useEffect } from "react";
+
+
+export function useIsInViewport(ref) {
+    const [isIntersecting, setIsIntersecting] = useState(false);
+  
+    const observer = useMemo(
+      () =>
+        new IntersectionObserver(([entry]) =>
+          setIsIntersecting(entry.isIntersecting),
+        ),
+      [],
+    );
+  
+    useEffect(() => {
+      observer.observe(ref.current);
+  
+      return () => {
+        observer.disconnect();
+      };
+    }, [ref, observer]);
+  
+    return isIntersecting;
+  }
