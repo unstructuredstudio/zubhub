389e4ccb20a92e4e559c5bd7d7f11bb88c1fbbfc
Aqsa Aqeel <aqsa.aqeel17@example.com>
2023-10-09 16:53:37 +0530

    fix sidenav to view port

--- zubhub_frontend/zubhub/src/components/Sidenav/sidenav.styles.js
+++ zubhub_frontend/zubhub/src/components/Sidenav/sidenav.styles.js
@@ -3,10 +3,10 @@ import { colors } from "../../assets/js/colors";
 export const sidenavStyle = theme => ({
     container: {
         backgroundColor: 'white',
-        minHeight: '80vh',
+        minHeight: '65vh',
         display: 'flex',
         borderRadius: 8,
-
+        position: 'fixed',
         [theme.breakpoints.down('sm')]: {
             height: '100%',
         },
