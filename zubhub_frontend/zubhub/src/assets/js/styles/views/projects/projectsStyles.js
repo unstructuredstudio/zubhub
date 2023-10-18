16b99aad63756ee618b4431d7865168ac18037a5
Berra Karaman <esma.berra.karaman@hotmail.com>
2023-10-18 17:13:00 +0300

    Revert "fix scrollbar bug"
    This reverts commit 1acd87ea8fd12fe93be0d6afabf04e4f84477d88.

--- zubhub_frontend/zubhub/src/assets/js/styles/views/projects/projectsStyles.js
+++ zubhub_frontend/zubhub/src/assets/js/styles/views/projects/projectsStyles.js
@@ -37,7 +37,7 @@ const styles = theme => ({
     paddingTop: '2.3em',
     paddingBottom: '2.85em',
     justifyContent: 'center',
-    width: '100%',
+    width: '100vw',
     marginBottom: '2em'
   },
   imageLeft: {
