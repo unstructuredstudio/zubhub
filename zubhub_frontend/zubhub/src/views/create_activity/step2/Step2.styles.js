ebf85c0cfdb86594cd77e85f56183f2fb756ba82
Steve Yonkeu <yokwejuste@yahoo.com>
2023-10-19 03:36:53 +0100

    feat: new style for activity creation (#915)

--- zubhub_frontend/zubhub/src/views/create_activity/step2/Step2.styles.js
+++ zubhub_frontend/zubhub/src/views/create_activity/step2/Step2.styles.js
@@ -4,5 +4,6 @@ export const step2Styles = theme => ({
         flexDirection: 'column',
         gap: 40
     },
-    formItem: { display: 'flex', flexDirection: 'column', gap: 20 }
+    formItem: { display: 'flex', flexDirection: 'column', gap: 20 },
+    stepTitle: { fontSize: 16, fontWeight: 400, margin: '20px 0'},
 })
