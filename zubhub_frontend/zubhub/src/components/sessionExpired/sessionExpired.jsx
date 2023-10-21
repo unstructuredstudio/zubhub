1cec7488818afd19c74a297a05625cd9476d65ef
Steve Yonkeu <yokwejuste@yahoo.com>
2023-10-21 04:32:32 +0000

    refactor: redirection text editted

--- zubhub_frontend/zubhub/src/components/sessionExpired/sessionExpired.jsx
+++ zubhub_frontend/zubhub/src/components/sessionExpired/sessionExpired.jsx
@@ -43,7 +43,7 @@ const SessionExpiredCard = () => {
                                 primaryButtonStyle
                                 customButtonStyle
                             >
-                                Login
+                                OK
                             </CustomButton>
                         </Link>
                     </div>
