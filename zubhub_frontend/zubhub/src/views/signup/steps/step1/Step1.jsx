7eff6288b19c3e614627675dbf33b015e6b1d472
gift256 <giftnamulika@gmail.com>
2023-12-20 09:28:48 +0300

    more signup page build up

--- zubhub_frontend/zubhub/src/views/signup/steps/step1/Step1.jsx
+++ zubhub_frontend/zubhub/src/views/signup/steps/step1/Step1.jsx
@@ -1,44 +1,56 @@
 import React from 'react';
+import clsx from 'clsx';
 import { Box, Button, Grid, Paper, Typography } from '@mui/material';
 import { FaUserGraduate, FaChalkboardUser } from 'react-icons/fa6';
 import { makeStyles } from '@mui/styles';
-import { step1Styles } from '../../signupStyles';
+import { mainStyles, step1Styles } from '../../signupStyles';
+import { CustomButton } from '../../../../components';
 
-const useStyles = makeStyles(step1Styles);
+const useMainStyles = makeStyles(mainStyles);
+const useStepStyles = makeStyles(step1Styles);
 
 const Step1 = props => {
-  const classes = useStyles();
+  const mainClasses = useMainStyles();
+  const classes = useStepStyles();
   return (
     <Box sx={{ width: '100%' }}>
-      <Grid container className={classes.gridContainer} rowSpacing={{ xs: 4, sm: 6, md: 10 }}>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        className={classes.gridContainer}
+        rowSpacing={{ xs: 4, sm: 6, md: 10 }}
+      >
         <Grid item>
-          <Typography align="center" className={classes.header}>
+          <Typography align="center" className={mainClasses.header}>
             Welcome to ZubHub !
           </Typography>
-          <Typography align="center" mt={2} className={classes.subHeader}>
+          <Typography align="center" mt={2} className={mainClasses.text}>
             Connect with students, educators, and the ZubHub community. To get started, choose an account type.
           </Typography>
         </Grid>
         <Grid item sx={{ width: '100%' }}>
-          <Grid container justifyContent="space-around">
-            <Grid>
-              <Paper variant="elevation" elevation={2} className={classes.paper}>
+          <Grid container justifyContent="space-around" rowGap={2}>
+            <Grid item>
+              <Paper variant="elevation" elevation={2} className={clsx([classes.paper, 'selected'])}>
                 <FaUserGraduate className={classes.icon} />
-                <Typography>Creator</Typography>
-                <Typography>I am a Creator, who wants to create</Typography>
+                <Typography className={mainClasses.subHeader}>Creator</Typography>
+                <Typography className={mainClasses.text}>I am a Creator, who wants to create</Typography>
               </Paper>
             </Grid>
-            <Grid>
+            <Grid item>
               <Paper className={classes.paper} variant="elevation" elevetion={2}>
                 <FaChalkboardUser className={classes.icon} />
-                <Typography>Educator</Typography>
-                <Typography>I am an Educator, looking to inspire</Typography>
+                <Typography className={mainClasses.subHeader}>Educator</Typography>
+                <Typography className={mainClasses.text}>I am an Educator, looking to inspire</Typography>
               </Paper>
             </Grid>
           </Grid>
         </Grid>
         <Grid item>
-          <Button className={classes.button}>JOIN AS A CREATOR</Button>
+          <CustomButton className={classes.button} primaryButtonStyle>
+            JOIN AS A CREATOR
+          </CustomButton>
         </Grid>
       </Grid>
     </Box>
--- zubhub_frontend/zubhub/src/views/signup/steps/step3/Step3.jsx
+++ zubhub_frontend/zubhub/src/views/signup/steps/step3/Step3.jsx
@@ -1,47 +1,85 @@
 // import { ArrowBackOutlined, LocationOnOutlined } from '@mui/icons-material';
-import { TfiLocationPin, TfiArrowLeft } from 'react-icons/tfi';
+import { TfiLocationPin, TfiArrowLeft, TfiAngleDown, TfiAngleUp } from 'react-icons/tfi';
 import {
   Box,
   Button,
   FormControl,
   Grid,
+  IconButton,
   InputAdornment,
   InputLabel,
   MenuItem,
+  OutlinedInput,
   Select,
   Typography,
 } from '@mui/material';
-import { CustomErrorMessage } from '../../../../components';
+import { CustomButton, CustomErrorMessage } from '../../../../components';
+import SpaceBackground from '../../../../assets/images/space.png';
+import classNames from 'classnames';
+import { makeStyles } from '@mui/styles';
+import { mainStyles, step3Styles } from '../../signupStyles';
+
+const useMainStyles = makeStyles(mainStyles);
+const useStyles = makeStyles(step3Styles);
 
 const Step3 = props => {
+  const mainClasses = useMainStyles();
+  const classes = useStyles();
+
   return (
-    <Box>
-      <Grid container direction="column">
-        <Grid>
-          <TfiArrowLeft />
-          <Grid>Image background</Grid>
+    <Box width="100%">
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        maxWidth="600px"
+        margin="0 auto"
+        rowSpacing={{ xs: 2, sm: 3, md: 5 }}
+      >
+        <Grid item>
+          <Grid position="absolute" mt={5} ml={5}>
+            <IconButton className={classes.backContainer}>
+              <TfiArrowLeft className={classes.backIcon} />
+            </IconButton>
+          </Grid>
+          <Grid>
+            <img src={SpaceBackground} alt="" className={classes.spaceBackground} />
+          </Grid>
         </Grid>
-        <Grid>
-          <Typography>What country do you live in?</Typography>
+        <Grid item>
+          <Typography maxWidth="342px" align="center" className={mainClasses.header2}>
+            What country do you live in?
+          </Typography>
         </Grid>
-        <Grid>
-          <FormControl>
-            <InputLabel>Select a location</InputLabel>
+        <Grid item width="100%">
+          <FormControl fullWidth>
             <Select
+              labelId="select-label"
               name="location"
+              input={<OutlinedInput />}
+              className={mainClasses.outlinedInput}
+              IconComponent={TfiAngleDown}
               startAdornment={
                 <InputAdornment>
                   <TfiLocationPin />
                 </InputAdornment>
               }
             >
-              <MenuItem>None</MenuItem>
+              <MenuItem value="">
+                <em>Select a location</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
             </Select>
             <CustomErrorMessage name="location" {...props} />
           </FormControl>
         </Grid>
-        <Grid>
-          <Button>Next</Button>
+        <Grid item width="100%">
+          <CustomButton primaryButtonStyle fullWidth className={mainClasses.button}>
+            Next
+          </CustomButton>
         </Grid>
       </Grid>
     </Box>
