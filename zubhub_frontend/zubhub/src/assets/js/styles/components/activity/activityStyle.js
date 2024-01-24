8ccd35c5203c77ac13917c92b8efa336ad642997
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-01-24 22:58:02 +0300

    redesigned activity card

--- zubhub_frontend/zubhub/src/assets/js/styles/components/activity/activityStyle.js
+++ zubhub_frontend/zubhub/src/assets/js/styles/components/activity/activityStyle.js
@@ -3,13 +3,13 @@ export const style = theme => ({
     position: 'relative',
     // maxWidth: '350px',
     // minWidth: '300px',
-    height: '95%',
   },
   activityCard: {
     maxWidth: '100%',
-    borderRadius: '15px',
-    position: 'relative!important',
     height: '100%',
+    minHeight: '34em',
+    borderRadius: '20px',
+    position: 'relative!important',
   },
   opacity: {
     backgroundColor: 'black',
@@ -29,56 +29,22 @@ export const style = theme => ({
   },
   mediaBoxStyle: {
     width: '100%',
-    height: '17em',
+    height: '14em',
     position: 'relative',
     padding: '2%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  activityTagsBox: {
-    position: 'absolute',
-    top: '10px',
-    right: '10%',
-    display: 'flex',
-  },
-  activityTagPill: {
-    backgroundColor: 'white',
-    color: 'var(--text-color2)',
-    border: '1px solid var(--text-color2)',
-    '&:hover': {
-      backgroundColor: 'var(--text-color2)',
-      color: 'white',
-      border: '1px solid white',
-    },
-  },
-  activityTagsShowMore: {
-    '&:hover': {
-      backgroundColor: 'white',
-      color: 'var(--text-color2)',
-      border: '1px solid white',
-    },
-  },
-  tagsShowMoreIconContainer: {
-    //position: 'absolute',
-  },
-  tagsShowMoreList: {
-    position: 'absolute',
-    right: '0%',
-    backgroundColor: 'white',
-    maxHeight: '12em',
-    overflow: 'auto',
-    borderRadius: '10px',
-  },
-
   activityCardContent: {
     width: '100%',
     position: 'relative',
-  },
-  activityCardInfoBox: {
-    height: '100%',
+    padding: '6px',
     display: 'flex',
-    justifyContent: 'space-between',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'start',
+    gap: '8px'
   },
   projectsCount: {
     color: 'white',
@@ -92,10 +58,107 @@ export const style = theme => ({
     marginLeft: '5px',
   },
   activityTitle: {
-    fontSize: '1.1rem',
-    fontWeight: '900',
+    fontSize: '1.4rem',
+    fontWeight: 700,
     color: 'var(--text-color1)',
-    // width: '80%',
-    textAlign: '-webkit-auto',
   },
+  activityDescription: {
+    fontSize: '1rem',
+    fontWeight: 500,
+    textAlign: 'start',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+  },
+  activityCategoryContainer: {
+    margin: '8px 0',
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '8px',
+  },
+  activityCategory: {
+    padding: '2px 10px',
+    border: '1px solid #7E5B4B',
+    borderRadius: '10em',
+    background: '#F1D27C'
+  },
+
+  activityCreatorContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '4px',
+    alignItems: 'start'
+  },
+  creatorBoxContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  creatorBox: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    gap: '15px'
+  },
+  creatorAvatar: {
+    boxShadow: `0 3px 5px 2px rgba(0, 0, 0, .12)`,
+  },
+  creatorUsername: {
+    flex: 1,
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    fontWeight: '600',
+    fontSize: '1.1em'
+  },
+  creatorUsernameTooltip: {
+    maxWidth: '70%',
+    marginRight: 'auto',
+    fontWeight: '700',
+  },
+  creatorTag: {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    fontWeight: '500',
+    fontSize: '1rem',
+    textAlign: 'center',
+  },
+
+  footer: {
+    width: '100%',
+    marginTop: 10,
+    flexDirection: 'row',
+    display: 'flex',
+  },
+  captionStyle: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  captionIconStyle: {
+    backgroundColor: '#eee',
+    padding: '2px 7px',
+    borderRadius: 25,
+    justifyContent: 'space-between',
+    fontWeight: '600',
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '1em',
+    '& svg': {
+      fill: 'rgba(0,0,0,0.54)',
+      marginRight: '0.5em',
+      fontSize:'1.1rem',
+    },
+  },
+  date: {
+    fontSize: '0.9rem',
+    fontWeight: '600',
+    marginLeft: 'auto'
+  }
 });
--- zubhub_frontend/zubhub/src/components/activity/activity.jsx
+++ zubhub_frontend/zubhub/src/components/activity/activity.jsx
@@ -5,8 +5,6 @@ import { connect } from 'react-redux';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import { style } from '../../assets/js/styles/components/activity/activityStyle';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 import {
   getActivities,
   activityToggleSave,
@@ -19,38 +17,23 @@ import {
   CardMedia,
   Typography,
   Box,
-  List,
-  ListItem,
-  ListItemText,
   Grid,
-  Fab,
+  Avatar,
+  Tooltip,
 } from '@material-ui/core';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 import commonStyles from '../../assets/js/styles';
-import Creator from '../creator/creator';
-import { toggleSave } from './activityScripts';
-import CustomButton from '../button/Button';
+import { dFormatter } from '../../assets/js/utils/scripts';
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 
 const useCommonStyles = makeStyles(commonStyles);
 const useStyles = makeStyles(style);
 function Activity(props) {
   const { activity, t } = { ...props };
-  const [tagsShowMore, setTagsShowMore] = useState(false);
   const classes = useStyles();
   const common_classes = useCommonStyles();
-  const topMarginCoefficient = activity.creator?.length < 6 ? 2 : 1;
-  const [state, handleSetState] = useState({ loading: false });
-
   return (
     <div className={classes.activityCardContainer}>
-      {activity.creators?.length > 0
-        ? activity.creators.map((creator, index) => (
-            <Creator
-              key={index}
-              creator={creator}
-              top={index * topMarginCoefficient - 1 + 'em'}
-            />
-          ))
-        : ''}
       <Link
         to={`/activities/${activity.id}`}
         className={common_classes.textDecorationNone}
@@ -63,128 +46,89 @@ function Activity(props) {
               alt={activity.title}
               className={classes.activityCardImage}
             />
-
-            <Box className={classes.activityTagsBox}>
-              {activity.tags?.length > 0
-                ? activity.tags.slice(0, 3).map(tag => (
-                    <Typography
-                      className={
-                        common_classes.baseTagStyle +
-                        ' ' +
-                        classes.activityTagPill
-                      }
-                      key={tag.id}
-                    >
-                      {tag.name}
-                    </Typography>
-                  ))
-                : ''}
-              {activity.tags?.length > 3 ? (
-                <div
-                  className={classes.tagsShowMoreIconContainer}
-                  onMouseOver={() => setTagsShowMore(true)}
-                  onMouseOut={() => setTagsShowMore(false)}
-                >
-                  <Typography
-                    className={
-                      common_classes.baseTagStyle +
-                      ' ' +
-                      classes.activityTagsShowMore
-                    }
-                    key="activityTagsShowMore"
-                  >
-                    {['+', activity.tags.length - 3].join('')}
-                  </Typography>
-                </div>
-              ) : (
-                ''
-              )}
-            </Box>
-
-            {tagsShowMore ? (
-              <Box
-                className={classes.tagsShowMoreList}
-                onMouseEnter={() => setTagsShowMore(true)}
-                onMouseLeave={() => setTagsShowMore(false)}
-              >
-                <List
-                  sx={{
-                    width: '100%',
-                    maxWidth: '150px',
-                    backgroundColor: 'background.paper',
-
-                    '& ul': { padding: 0 },
-                  }}
-                >
-                  {activity.tags?.map(tag => (
-                    <ListItem key={`tag-${tag.id}`}>
-                      <ListItemText primary={tag.name} />
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-            ) : (
-              ''
-            )}
           </CardMedia>
           <CardActions>
             <CardContent className={classes.activityCardContent}>
-              <Fab
-                className={common_classes.fabButtonStyle}
-                size="small"
-                aria-label="save button"
-                onClick={e =>
-                  toggleSave(
-                    e,
-                    activity.id,
-                    props.auth,
-                    props.history,
-                    handleSetState,
-                    props.activityToggleSave,
-                    t,
-                  )
-                }
+              <Typography
+                // variant="h6"
+                // component="h6"
+                className={classes.activityTitle}
               >
-                {props.auth && activity.saved_by.includes(props.auth.id) ? (
-                  <BookmarkIcon aria-label="unsave" />
-                ) : (
-                  <BookmarkBorderIcon aria-label="save" />
-                )}
-              </Fab>
-              <Grid
-                container
-                className={clsx(
-                  classes.activityCardInfoBox,
-                  common_classes.alignCenter,
-                )}
+                {activity.title}
+              </Typography>
+              <Typography
+                color="textSecondary"
+                className={classes.activityDescription}
               >
-                <Grid item xs={6} sm={6}>
-                  <Typography
-                    variant="h6"
-                    component="h6"
-                    className={classes.activityTitle}
-                  >
-                    {activity.title}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Link
-                    to={`/activities/${activity.id}/linkedProjects`}
-                    className={common_classes.textDecorationNone}
-                    onClick={e => props.setActivity(activity)}
-                  >
-                    <Typography
-                      variant="caption"
-                      className={classes.projectsCount}
+                {activity.introduction.replace(/(<([^>]+)>)/gi, "")}
+              </Typography>
+              {activity.category.length > 0 && (
+                <div className={classes.activityCategoryContainer}>
+                  {activity.category.map(cat => (
+                    <div className={classes.activityCategory}>{cat}</div>
+                  ))}
+                </div>
+              )}
+              <div className={classes.activityCreatorContainer}>
+                {activity.creators?.length > 0 &&
+                  activity.creators.map(creator => (
+                    <Link
+                      to={`/creators/${creator.username}`}
+                      className={clsx(common_classes.textDecorationNone, classes.creatorBoxContainer)}
                     >
-                      {`${t('activities.LinkedProjects')} `}{' '}
-                      <span
-                        className={classes.projectsCountNumber}
-                      >{` ${activity.inspired_projects.length}`}</span>
-                    </Typography>
-                  </Link>
-                </Grid>
-              </Grid>
+                      <Box className={classes.creatorBox}>
+                        <Avatar 
+                          className={classes.creatorAvatar}
+                          src={creator.avatar}
+                          alt={creator.username}
+                        />
+                        <Tooltip
+                          title={creator.username}
+                          placement="bottom"
+                          arrow
+                          className={classes.creatorUsernameTooltip}
+                        >
+                          <Box>
+                            <Typography color="textSecondary" variant="caption" component="p" className={classes.creatorUsername}>
+                              {creator.username}
+                            </Typography>
+                          </Box>
+                        </Tooltip>
+                      </Box>
+                      <Typography color="textPrimary" className={classes.creatorTag} component="p">
+                        {creator.tags[0]}
+                      </Typography>
+                    </Link>
+                  ))
+                }
+              </div>
+              <Box className={classes.footer}>
+                <Box className={classes.captionStyle}>
+                  <Typography
+                    className={classes.captionIconStyle}
+                    color="textSecondary"
+                    variant="caption"
+                    component="span"
+                  >
+                    <VisibilityIcon /> {activity.views_count}
+                  </Typography>
+                  <Typography
+                    className={classes.captionIconStyle}
+                    color="textSecondary"
+                    variant="caption"
+                    component="span"
+                  >
+                    <EmojiObjectsIcon /> {activity.inspired_projects.length}
+                  </Typography>
+                </Box>
+                <Typography color="textSecondary" variant="caption" component="span" className={classes.date}>
+                  {`
+                    ${dFormatter(activity.created_on).value} 
+                    ${t(`date.${dFormatter(activity.created_on).key}`)} 
+                    ${t('date.ago',)}
+                  `}
+                </Typography>
+              </Box>
             </CardContent>
           </CardActions>
         </Card>
