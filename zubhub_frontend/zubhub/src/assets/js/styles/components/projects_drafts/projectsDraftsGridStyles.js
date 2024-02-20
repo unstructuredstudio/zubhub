fe24916323990807868d38191bf278a0557e2b56
Ndibe Raymond Olisaemeka <rolisaemeka-ctr@wikimedia.org>
2024-02-20 11:04:52 +0100

    (fix): Creator profile gets margin * fix creator profile margin * fix projects section margin
    Closes #1005

--- zubhub_frontend/zubhub/src/assets/js/styles/components/projects_drafts/projectsDraftsGridStyles.js
+++ zubhub_frontend/zubhub/src/assets/js/styles/components/projects_drafts/projectsDraftsGridStyles.js
@@ -1,4 +1,4 @@
-const styles = theme => ({
+const styles = () => ({
   titleStyle: {
     fontWeight: 900,
     fontSize: '1.5rem',
@@ -7,10 +7,11 @@ const styles = theme => ({
 
   profileLowerStyle: {
     // margin: '1em',
-    [theme.breakpoints.down('740')]: {
-      margin: '1em',
-    },
-    borderRadius: 8, overflow: 'hidden'
+    // [theme.breakpoints.down('740')]: {
+    //   margin: '1em',
+    // },
+    borderRadius: 8,
+    overflow: 'hidden',
   },
 
   viewAllBtn: {
@@ -22,21 +23,21 @@ const styles = theme => ({
   },
 
   tabStyle: {
-      '&.Mui-selected': {
-        color: 'var(--primary-color3)',
-        fontWeight: 'bold',
-        backgroundColor: '#ffffff',
-      },
+    '&.Mui-selected': {
+      color: 'var(--primary-color3)',
+      fontWeight: 'bold',
+      backgroundColor: '#ffffff',
+    },
 
-      backgroundColor: 'var(--text-color3)',
-      width: '50%',
-      maxWidth: '50%',
+    backgroundColor: 'var(--text-color3)',
+    width: '50%',
+    maxWidth: '50%',
   },
 
   indicator: {
-    backgroundColor: "var(--primary-color3)",
+    backgroundColor: 'var(--primary-color3)',
     height: '5px',
-  }
+  },
 });
 
 export default styles;
