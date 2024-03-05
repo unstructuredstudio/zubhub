a0d6c68f8e8b7e305b7d711af80353868ab3c8e4
Steve Yonkeu <yokwejuste@yahoo.com>
2024-03-05 17:09:59 +0100

    rebase

--- zubhub_backend/zubhub/activities/views.py
+++ zubhub_backend/zubhub/activities/views.py
@@ -1,16 +1,17 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML
 from rest_framework.generics import (
     ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView)
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from .permissions import IsStaffOrModeratorOrEducator, IsOwner, IsStaffOrModerator
 from django.shortcuts import get_object_or_404
 from .models import *
 from .serializers import *
 from django.db import transaction
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.models import AnonymousUser
-from .utils import generate_pdf, generate_qr_code, download_file
-from django.conf import settings
+
 
 
 class ActivityListAPIView(ListAPIView):
@@ -21,7 +22,7 @@ class ActivityListAPIView(ListAPIView):
     def get_queryset(self):
         all = Activity.objects.all()
         return all
-
+    
 
 class UserActivitiesAPIView(ListAPIView):
     """
@@ -31,7 +32,7 @@ class UserActivitiesAPIView(ListAPIView):
 
     serializer_class = ActivitySerializer
     permission_classes = [IsAuthenticated, IsOwner]
-
+    
     def get_queryset(self):
         return self.request.user.activities_created.all()
 
@@ -51,7 +52,7 @@ class ActivityDetailsAPIView(RetrieveAPIView):
         queryset = self.get_queryset()
         pk = self.kwargs.get("pk")
         obj = get_object_or_404(queryset, pk=pk)
-
+        
         if obj:
             with transaction.atomic():
                 if isinstance(self.request.user, AnonymousUser):
@@ -63,7 +64,7 @@ class ActivityDetailsAPIView(RetrieveAPIView):
                         obj.views_count += 1
                         obj.save()
             return obj
-
+    
         else:
             raise Exception()
 
@@ -75,7 +76,7 @@ class PublishedActivitiesAPIView(ListAPIView):
 
     serializer_class = ActivitySerializer
     permission_classes = [AllowAny]
-
+    
     def get_queryset(self):
         limit = self.request.query_params.get('limit', 10000)
 
@@ -85,7 +86,7 @@ class PublishedActivitiesAPIView(ListAPIView):
             limit = 10
 
         return Activity.objects.filter(publish= True)[:limit]
-
+    
 class UnPublishedActivitiesAPIView(ListAPIView):
     """
     Fetch list of unpublished activities by authenticated staff member.
@@ -98,7 +99,7 @@ class UnPublishedActivitiesAPIView(ListAPIView):
     permission_classes = [IsAuthenticated, IsStaffOrModerator]
 
     def get_queryset(self):
-        return Activity.objects.filter(publish= False)
+        return Activity.objects.filter(publish= False)  
 
 class ActivityCreateAPIView(CreateAPIView):
     """
@@ -148,11 +149,11 @@ class ToggleSaveAPIView(RetrieveAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
     permission_classes = [IsAuthenticated]
-
+  
     def get_object(self):
         pk = self.kwargs.get("pk")
         obj = get_object_or_404(self.get_queryset(), pk=pk)
-
+       
         if self.request.user in obj.saved_by.all():
             obj.saved_by.remove(self.request.user)
             obj.save()
@@ -171,30 +172,27 @@ class togglePublishActivityAPIView(RetrieveAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
     permission_classes = [IsAuthenticated, IsStaffOrModerator]
-
+   
 
     def get_object(self):
-
+       
         pk = self.kwargs.get("pk")
-        obj = get_object_or_404(self.get_queryset(), pk=pk)
+        obj = get_object_or_404(self.get_queryset(), pk=pk)  
         obj.publish = not obj.publish
         obj.save()
         return obj
 
 
 
-class DownloadActivityPDF(APIView):
+class DownloadActivityPDF(RetrieveAPIView):
     """
     Download an activities.
     Requires activities id.
     Returns activities file.
     """
     queryset = Activity.objects.all()
-    template_path = 'activities/activity_download.html'
-
-
-    def get_queryset(self):
-        return self.queryset
+    permission_classes = [IsAuthenticated]
+    serializer_class = ActivitySerializer
 
     def get_object(self):
         pk = self.kwargs.get("pk")
@@ -203,27 +201,15 @@ class DownloadActivityPDF(APIView):
 
     def get(self, request, *args, **kwargs):
         activity = self.get_object()
-        activity_images = ActivityImage.objects.filter(activity=activity)
-        activity_steps = ActivityMakingStep.objects.filter(activity=activity)
-        if settings.ENVIRONMENT == 'production':
-            qr_code = generate_qr_code(
-                link=f"https://zubhub.unstructured.studio/activities/{activity.id}"
-            )
-        else:
-            qr_code = generate_qr_code(
-                link=f"{settings.DEFAULT_BACKEND_PROTOCOL}//{settings.DEFAULT_BACKEND_DOMAIN}/activities/{activity.id}"
-            )
+
         context = {
             'activity': activity,
-            'activity_id': activity.id,
-            'activity_images': activity_images,
-            'activity_steps': activity_steps,
-            'activity_steps_images': [step.image.all() for step in activity_steps],
-            'activity_category': [category.name for category in activity.category.all()],
-            'creators': [creator for creator in activity.creators.all()],
-            'qr_code': qr_code
         }
-        return generate_pdf(
-            template_path=self.template_path,
-            context=context
-        )
+        html_string = render_to_string('activities/activity_download.html', context)
+
+        html = HTML(string=html_string)
+        pdf = html.write_pdf()
+
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'filename="{activity.id}.pdf"'
+        return response
